2773d7c07c53655ba906fb1a0fdb2f8f824032de
1270304478 <1270304478@qq.com>
2017-12-19 16:41:02 +0800

    Initial commit

---
+++ app/src/main/java/com/bwei/titleactivitydemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bwei.titleactivitydemo;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends TitleActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/bwei/titleactivitydemo/TitleActivity.java
@@ -0,0 +1,112 @@
+package com.bwei.titleactivitydemo;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+public class TitleActivity extends Activity implements View.OnClickListener {
+    private TextView mTitleTextView;
+    private Button mBackwardbButton;
+    private Button mForwardButton;
+    private FrameLayout mContentLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupViews();   //加载 activity_title 布局 ，并获取标题及两侧按钮
+    }
+    private void setupViews() {
+        super.setContentView(R.layout.activity_title);
+        mTitleTextView = (TextView) findViewById(R.id.text_title);
+        mContentLayout = (FrameLayout) findViewById(R.id.layout_content);
+        mBackwardbButton = (Button) findViewById(R.id.button_backward);
+        mForwardButton = (Button) findViewById(R.id.button_forward);
+    }
+
+
+    protected void showBackwardView(int backwardResid, boolean show) {
+        if (mBackwardbButton != null) {
+            if (show) {
+                mBackwardbButton.setText(backwardResid);
+                mBackwardbButton.setVisibility(View.VISIBLE);
+            } else {
+                mBackwardbButton.setVisibility(View.INVISIBLE);
+            }
+        } // else ignored
+    }
+
+
+    protected void showForwardView(int forwardResId, boolean show) {
+        if (mForwardButton != null) {
+            if (show) {
+                mForwardButton.setVisibility(View.VISIBLE);
+                mForwardButton.setText(forwardResId);
+            } else {
+                mForwardButton.setVisibility(View.INVISIBLE);
+            }
+        } // else ignored
+    }
+
+
+
+    //设置标题内容
+    @Override
+    public void setTitle(int titleId) {
+        mTitleTextView.setText(titleId);
+    }
+
+    //设置标题内容
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitleTextView.setText(title);
+    }
+
+    //设置标题文字颜色
+    @Override
+    public void setTitleColor(int textColor) {
+        mTitleTextView.setTextColor(textColor);
+    }
+
+
+    //取出FrameLayout并调用父类removeAllViews()方法
+    @Override
+    public void setContentView(int layoutResID) {
+        mContentLayout.removeAllViews();
+        View.inflate(this, layoutResID, mContentLayout);
+        onContentChanged();
+    }
+
+    @Override
+    public void setContentView(View view) {
+        mContentLayout.removeAllViews();
+        mContentLayout.addView(view);
+        onContentChanged();
+    }
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        mContentLayout.removeAllViews();
+        mContentLayout.addView(view, params);
+        onContentChanged();
+    }
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.button_backward:
+                Toast.makeText(this, "点击返回按钮可在此调用finish()", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.button_forward:
+                Toast.makeText(this, "我是提交按钮", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+}
